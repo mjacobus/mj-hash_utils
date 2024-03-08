3d1c6f8429544b1c9e927f288fd558b95f4d7579
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2024-03-08 14:45:30 -0300

    Initial setup

---
+++ lib/mj/hash_utils.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "hash_utils/version"
+
+module Mj
+  module HashUtils
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/mj/hash_utils_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RSpec.describe Mj::HashUtils do
+  it "has a version number" do
+    expect(Mj::HashUtils::VERSION).not_to be nil
+  end
+end
